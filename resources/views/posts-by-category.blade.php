e10f90faff296cc3cf48442b9cd3bca484793b0d
dimasgilang <105497804+ddim03@users.noreply.github.com>
2023-11-08 22:35:54 +0700

    (feat/post-by-category):membuat halaman post by category & menerapkan layouts di views (#15)

--- resources/views/posts-by-category.blade.php
+++ resources/views/posts-by-category.blade.php
@@ -2,44 +2,57 @@
 
 @section('content')
 @include('partials.navbar')
-<section class="min-h-screen bg-[url('../img/hero.jpg')] bg-cover bg-center">
-    <div class="w-full h-screen bg-gray-900 bg-opacity-75 flex items-center ">
+
+<section class="h-80 md:h-96 bg-[url('../img/hero.jpg')] bg-cover bg-center">
+    <div class="w-full h-80 md:h-96 bg-gray-900 bg-opacity-75 flex items-center ">
         <div class="w-11/12 sm:w-4/5 mx-auto">
             <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16" id="search">
                 <h1
-                    class="mb-4 mt-20 text-3xl sm:text-4xl font-extrabold tracking-tight leading-tight text-white md:text-5xl lg:text-6xl">
-                    Sistem Informasi Pengumuman Mahasiswa</h1>
+                    class="mb-6 mt-20 text-3xl sm:text-4xl font-extrabold tracking-tight leading-tight text-white md:text-5xl lg:text-6xl">
+                    Pengumuman Akademik</h1>
                 <p class="mb-8 text-md sm:text-lg font-normal text-gray-50 lg:text-xl sm:px-16 lg:px-48">
-                    Platform pengumuman untuk mahasiswa jurusan Manajemen Informatika
-                    PSDKU POLINEMA di Kota Kediri</p>
-                <div class="w-full md:w-4/5 mx-auto">
-                    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
-                    <div class="relative">
-                        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                            <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                fill="none" viewBox="0 0 20 20">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                            </svg>
-                        </div>
-                        <input type="search" id="default-search"
-                            class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-                            placeholder="Cari judul pengumuman...">
-                    </div>
-                </div>
+                    Pengumuman tentang kegiatan akademik mahasiswa program studi manajemen informatika</p>
             </div>
         </div>
     </div>
 </section>
-<section class="w-full md:w-4/5 px-3 md:px-0 pt-8 mx-auto max-w-screen-xl">
-    <div class="flex justify-between items-end">
-        <div class="grub">
-            <h2 class="text-2xl font-semibold text-gray-800">Terbaru</h2>
-            <p class="text-md text-gray-400">Pengumuman yang baru saja dipublikasikan</p>
+<section class="w-full md:w-4/5 px-3 md:px-0 pt-6 mx-auto max-w-screen-xl">
+    <nav class="flex" aria-label="Breadcrumb">
+        <ol class="inline-flex items-center space-x-1 md:space-x-3">
+            <li class="inline-flex items-center">
+                <a href="#"
+                    class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                    Beranda
+                </a>
+            </li>
+            <li>
+                <div class="flex items-center">
+                    <svg class="w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                        fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 9 4-4-4-4" />
+                    </svg>
+                    <a href="#"
+                        class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">Akademik</a>
+                </div>
+            </li>
+        </ol>
+    </nav>
+    <div class="flex mt-4 ">
+        <div class="w-full md:w-1/2">
+            <div class="relative">
+                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    </svg>
+                </div>
+                <input type="search" id="default-search"
+                    class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="Cari judul pengumuman...">
+            </div>
         </div>
-        <a href="#"
-            class="px-3 py-2 text-sm font-medium text-center text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300">Lihat
-            semua</a>
     </div>
     <div class="w-full grid grid-cols-1 lg:grid-cols-2 mt-5 mb-14 gap-3">
         <a href="#"
