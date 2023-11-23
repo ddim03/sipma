ba689619c21214c63afa487e9643fd4b65ede828
kyutties <150522096+kyutties@users.noreply.github.com>
2023-11-23 21:22:33 +0700

    fix/model tabel pengumuman (#40)
    * (fix/model-tabel-pengumuman):setup pengumuman
    
    * (fix/model-tabel-pengumuman):setup pengumuman

---
+++ database/migrations/2023_11_22_054013_create_admin_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admin', function (Blueprint $table) {
+            $table->id('admin_id');
+            $table->string('nama');
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->boolean('is_kaprodi')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admin');
+    }
+};
